b85d27d2c43df05ef2d37da4da9587bdfa1d05bf
Henry Vigor <henry.vigor@gmail.com>
2025-12-02 22:45:04 -0600

    ✨ Add solutions for day 1

---
+++ 01/p1/src/main.cpp
@@ -0,0 +1,26 @@
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "input/getFirstFile.h"
+
+int main() {
+    std::ifstream file = getFirstFile();
+    if (!file) return 1;
+
+    std::string line;
+    int dial = 50;
+    int zeros = 0;
+    while (!file.eof()) {
+        std::getline(file, line);
+        char dir = line[0];
+        int delta = std::stoi(line.substr(1, line.size() - 1));
+
+        dial = (dir == 'R' ? dial + delta : dial - delta) % 100;
+
+        zeros += dial == 0;
+    }
+
+    std::cout << "Solution: " << zeros << "\n";
+}

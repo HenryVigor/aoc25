a25be700cc42160c855b2e65fa6f913ddf93b61c
Henry Vigor <henry.vigor@gmail.com>
2025-12-03 22:17:04 -0600

    🚨 Remove unnecessary lines
    Removes unused variable in `input/getFirstFile.cpp` in both parts of days 01 and 02

--- 02/p1/src/input/getFirstFile.cpp
+++ 02/p1/src/input/getFirstFile.cpp
@@ -7,7 +7,6 @@
 
 std::ifstream getFirstFile() {
     std::ifstream file;
-    std::string fileName;
     for (const auto& entry : std::filesystem::directory_iterator("input")) {
         file = std::ifstream(entry.path());
         break;
